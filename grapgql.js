e3c946db9bdf30768e46b55faba5378990073744
achantaprakash <118893794+Prakash-Achanta@users.noreply.github.com>
2023-11-24 22:06:01 +0530

    Initial Commit

---
+++ grapgql.js
@@ -0,0 +1,24 @@
+import { check } from 'k6';
+import http from 'k6/http'
+
+const query =`query MyQuery {
+    episode(id: "1") {
+      air_date
+      created
+      episode
+      name
+    }
+  }`
+
+  const headers = {
+    'Content-Type':'application/json'
+  };
+
+  export default function(){
+
+    const url = 'https://rickandmortyapi.com/graphql';
+    const payload = JSON.stringify({query})
+    const res = http.post(url,payload,headers)
+    check( res,{'Status Code': (r)=> r.status ===200});
+    console.log(res.body);
+  }
