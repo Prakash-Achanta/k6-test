b0e9f9eab52646a27e7133ec1f264941b896f1c7
achantaprakash <118893794+Prakash-Achanta@users.noreply.github.com>
2023-11-24 22:39:08 +0530

    Try with Extension

---
+++ testdashboard.js
@@ -0,0 +1,24 @@
+import http from 'k6/http';
+import { check } from 'k6'
+
+export const options = {
+    vus: 40,
+    duration: '60s',
+  };
+
+export default function(){
+    const url = "https://test-api.k6.io/public/crocodiles/"
+
+    const res = http.get(url);
+    check( res,{'Status Code': (r)=> r.status ===200});
+    const responseData = JSON.parse(res.body);
+    const data = responseData;
+
+    // Print the count of IDs
+    console.log(`Number of IDs: ${data.length}`);
+
+    // Print all the names
+    data.forEach((item) => {
+        console.log(`ID: ${item.id}, Name: ${item.name}`);
+    });
+}
