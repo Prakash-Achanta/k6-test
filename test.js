5d284ebd1135f586451e8403283576c076980677
achantaprakash <118893794+Prakash-Achanta@users.noreply.github.com>
2023-11-25 15:18:45 +0530

    fix/loadTest vu

--- test.js
+++ test.js
@@ -1,6 +1,11 @@
 import http from 'k6/http';
 import { check } from 'k6'
 
+export const options = {
+    vus: 10,
+    duration: '5s',
+  };
+
 
 export default function(){
     const url = "https://test-api.k6.io/public/crocodiles/"
@@ -8,13 +13,13 @@ export default function(){
     const res = http.get(url);
     check( res,{'Status Code': (r)=> r.status ===200});
     const responseData = JSON.parse(res.body);
-    const data = responseData;
+    // const data = responseData;
 
-    // Print the count of IDs
-    console.log(`Number of IDs: ${data.length}`);
+    // // Print the count of IDs
+    // console.log(`Number of IDs: ${data.length}`);
 
-    // Print all the names
-    data.forEach((item) => {
-        console.log(`ID: ${item.id}, Name: ${item.name}`);
-    });
+    // // Print all the names
+    // data.forEach((item) => {
+    //     console.log(`ID: ${item.id}, Name: ${item.name}`);
+    // });
 }
